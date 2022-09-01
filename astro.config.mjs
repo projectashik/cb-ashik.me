5db779d1f922fac9cf255ac4ef73359efd961e44
Ashik <projectashik@gmail.com>
2022-09-01 16:02:48 +0000

    added blogs pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,19 +12,19 @@ import remarkEmbedder from "@remark-embedder/core"
 import oembedTransformer from "@remark-embedder/transformer-oembed"
 
 const YTTransformer = {
-  name: 'YouTube',
+  name: "YouTube",
   // shouldTransform can also be async
   shouldTransform(url) {
-    const {host, pathname} = new URL(url)
+    const { host, pathname } = new URL(url)
 
     return (
-      ['youtube.com', 'www.youtube.com'].includes(host) &&
-      pathname.includes('/watch?v=')
+      ["youtube.com", "www.youtube.com"].includes(host) &&
+      pathname.includes("/watch?v=")
     )
   },
   // getHTML can also be async
   getHTML(url) {
-    const iframeUrl = url.replace('/watch?v=', '/embed/')
+    const iframeUrl = url.replace("/watch?v=", "/embed/")
 
     return `<iframe src="${iframeUrl}" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking" sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"></iframe>`
   },
